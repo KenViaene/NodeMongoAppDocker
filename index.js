2ba91777735125330441a1d5435777c183ec7139
kenvi <ken.viaene@student.vives.be>
2021-12-19 16:20:24 +0100

    oplEXTRA

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to MongoDB
+// Connect to MongoD
 /* mongoose
   .connect(
     'mongodb://mongo:27017/docker-node-mongo',
